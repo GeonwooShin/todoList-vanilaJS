550c56a95d73335739d6e52324e92ede111aa480
GeonwooShin <rjsdnslawkd@naver.com>
2022-03-24 01:05:39 +0900

    랜덤 배경화면 구현

---
+++ src/background.js
@@ -0,0 +1,8 @@
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"]
+
+const selectedImg = images[Math.floor(Math.random() * images.length)]
+
+const bgImg = document.createElement("img")
+bgImg.setAttribute('src', `img/${selectedImg}`)
+
+document.body.appendChild(bgImg)
