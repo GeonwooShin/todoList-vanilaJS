3d232adcaa734b5e5367602c22809f3f9c7aa63c
GeonwooShin <rjsdnslawkd@naver.com>
2022-03-27 18:40:43 +0900

    css 구현

--- src/greeting.js
+++ src/greeting.js
@@ -1,3 +1,5 @@
+import { todoForm } from "./todo.js"
+
 const nameForm = document.querySelector('.name__form')
 const nameInput = document.querySelector('.name__form-input')
 const nameInputBtn = document.querySelector('.name__form-button')
